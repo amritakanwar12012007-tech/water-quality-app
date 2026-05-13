8635c6df7f346b43f551cd226aaa2aeecc29342e
amritakanwar12012007-tech <amritakanwar12012007@gmail.com>
2026-05-13 19:40:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
- import streamlit as st   
-    import pandas as pd
+import streamlit as st   
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
